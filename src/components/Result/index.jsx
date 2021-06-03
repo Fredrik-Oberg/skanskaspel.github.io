a28e4f502bc3b3dcbe780871ba3dd36a377f1c59
Fredrik Öberg <fredrik.oberg@dooer.com>
2021-06-03 08:48:35 +0200

    Flag fixes + result number

--- src/components/Result/index.jsx
+++ src/components/Result/index.jsx
@@ -11,6 +11,7 @@ import {
   useTheme,
   makeStyles,
 } from "@material-ui/core";
+// import { getStorage, ref, getDownloadURL } from "firebase/storage";
 
 import Loader from "../Loader";
 
@@ -29,6 +30,51 @@ function Results({ firebase }) {
     async function getResults() {
       const get = firebase.functions.httpsCallable("results");
       const res = await get();
+      setResults(res.data);
+
+      // // Local json file
+      // const storageRef = firebase.storage.ref();
+      // // const storage = getStorage();
+      // var starsRef = storageRef.child("results");
+
+      // // Get the download URL
+      // const res = await starsRef
+      //   .getDownloadURL()
+      //   .then(async (url) => {
+      //     // Insert url into an <img> tag to "download"
+      //     const res = await fetch(url, {
+      //       headers: {
+      //         "Content-Type": "application/json",
+      //         Accept: "application/json",
+      //       },
+      //     }).then(function (response) {
+      //       return response.json();
+      //     });
+      //   })
+      //   .catch((error) => {
+      //     debugger;
+      //     // Handle any errors
+      //   });
+      // debugger;
+      // const res = await getDownloadURL(ref(storage, "bucket/results"))
+      //   .then(async (url) => {
+      //     // `url` is the download URL for 'images/stars.jpg'
+
+      //     // This can be downloaded directly:
+      //     const res = await fetch(url, {
+      //       headers: {
+      //         "Content-Type": "application/json",
+      //         Accept: "application/json",
+      //       },
+      //     }).then(function (response) {
+      //       return response.json();
+      //     });
+      //   })
+      //   .catch((error) => {
+      //     debugger;
+      //     // Handle any errors
+      //   });
+      // debugger;
       setResults(res.data);
     }
     getResults();
@@ -64,7 +110,7 @@ function Results({ firebase }) {
           {results.map((row, i) => (
             <TableRow key={row.name}>
               <TableCell colSpan={1}>
-                {i === 0 ? "🥇" : i === 1 ? "🥈" : ""}
+                {i === 0 ? "🥇" : i === 1 ? "🥈" : (i + 1).toString()}
               </TableCell>
               <TableCell colSpan={7}>{row.name}</TableCell>
               <TableCell align="center" colSpan={1}>
